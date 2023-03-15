0472c6e502450909efa39d83ce760676760e5a69
한규리 <pos7271@team-everywhere.com>
2023-03-15 16:45:51 +0900

    3.15

---
+++ level0/짝수의힘.js
@@ -0,0 +1,11 @@
+// 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을
+// 내풀이
+const even = (n) => {
+  let answer = 0;
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 === 0) answer += i;
+  }
+  return answer;
+};
+
+even(10);
